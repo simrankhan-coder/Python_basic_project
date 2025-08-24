fb81d1065c995cb4d8f7d874bc66e571d36d33af
simrankhan-coder <simrankhan10102@gmail.com>
2025-08-24 12:06:53 +0530

    code

--- simple_cal_project1.py
+++ simple_cal_project1.py
@@ -29,8 +29,36 @@ elif op=='/':
 elif op=='-':
     result="invalid operator"
 
-<<<<<<< HEAD
 print("Result:",result)
-=======
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    if y==0:
+        return "Error:Division by zero"
+    return x/y
+print("==Simple Calculator==")
+print("Do operation Like:")
+print("1.Add(+)")
+print("2.Subtract(-)")
+print("3.Multipy(*)")
+print("4.Divide(/)")
+op=input("Enter you choice(+,-,*,/):")
+a=float(input("Enter first num:"))
+b=float(input("Enter Second num:"))
+
+if op=='+':
+    result=add(a,b)
+elif op=='-':
+    result=subtract(a,b)
+elif op=='*':
+    result=multiply(a,b)
+elif op=='/':
+    result=divide(a,b)
+elif op=='-':
+    result="invalid operator"
+
 print("Result:",result)
->>>>>>> af6a4013b2cacd26946fb4df6a4c1deaed1cf04a
